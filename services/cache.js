392b6031f52b0254e95ea99532f8c5296d7a903a
NathanCotrim <nathan.cotrim@gmail.com>
2021-10-09 18:27:46 -0300

    feat: subscribing mongoose query.exec fn

---
+++ services/cache.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const exec = mongoose.Query.prototype.exec
+
+mongoose.Query.prototype.exec = function () {
+    console.log('Im about to run a query');   
+    
+    return exec.apply(this, arguments)
+}
