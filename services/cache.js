ddc46ce752b434b0c0ccb63c2147044fc3f3b659
NathanCotrim <nathan.cotrim@gmail.com>
2021-10-09 18:46:17 -0300

    fix: fix mongoose connection

--- services/cache.js
+++ services/cache.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec
 
 mongoose.Query.prototype.exec = function () {
-    console.log('Im about to run a query');   
-    
+    console.log('Im about to run a query');
+
+    console.log(this.getQuery());
+
     return exec.apply(this, arguments)
 }
